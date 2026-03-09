797881c6b6918ba8c3966bcc489652b0d918dde3
SajtyamMauryakit <sa9451736205@gmail.com>
2026-03-10 03:08:29 +0530

    fix the error and ok

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-   
+
+@app.route("/")
+def home():
+    """Render the home page."""
+    return render_template("index.html")
